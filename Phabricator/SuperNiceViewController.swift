60c460d541e26458c6a4f887e4241dbc2820226e
Alexandre Garrefa <alexmrg@gmail.com>
2018-11-22 11:10:59 +0100

    My really nice new super feature

---
+++ Phabricator/SuperNiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuperNiceViewController.swift
+//  Phabricator
+//
+//  Created by Alex on 22/11/2018.
+//  Copyright © 2018 Garrefa. All rights reserved.
+//
+
+import UIKit
+
+class SuperNiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
